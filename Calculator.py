0dc257141a10acdea27261c2ff810904c7692f96
Sanisacat <86495146+Sanisacat@users.noreply.github.com>
2021-09-21 15:39:05 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+print("Hello! Welcome to Calculator!")
+x = 10
+y = 12
+
+sum = x + y
+diff = x - y 
+product = x * y
+divide = x / y
+
+print("The sum of x and y is ", sum)
+print("The difference of x and y is ", diff)
+print("The product of x and y is ", product)
+print("The divide of x and y is ", divide)
+
+
